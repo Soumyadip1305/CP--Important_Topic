3966fff0755ca4a5396087e21a49f0b3f7b2792b
Soumyadip Saha <115612619+Soumyadip1305@users.noreply.github.com>
2023-04-21 15:31:47 +0530

    Update Triangle.java

--- src/Dynamic_Programming/CodeStudio/Triangle.java
+++ src/Dynamic_Programming/CodeStudio/Triangle.java
@@ -1,5 +1,7 @@
 package Dynamic_Programming.CodeStudio;
 
+//  https://www.codingninjas.com/codestudio/problems/triangle_1229398?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=1
+
 import java.util.Arrays;
 
 public class Triangle {
